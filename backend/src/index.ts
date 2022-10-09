b4051503a28e5e0d812a6d70644568ff88e48693
ehurafa <rafarfl2010@gmail.com>
2022-10-08 22:22:17 -0400

    routes config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
-import * as express from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+// type inference
+function getUsername() {
+    return 'rafa'
+}
+const name = getUsername() // return string
+
+app.use(routes);
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
